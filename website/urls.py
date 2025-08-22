a2c76959ecd4defe0f3a1dd4c7a5406bc1105330
eddyhgs <eddyhgs@gmail.com>
2025-08-22 12:48:47 -0300

    mudanças

--- website/urls.py
+++ website/urls.py
@@ -9,4 +9,5 @@ path('base/', views.base, name="base"),
 path('login/', views.login_user, name="login"),
 path('logout/', views.logout_user, name="logout"),
 path('register/', views.register_user, name="register"),
+path('registro/<int:pk>/', views.registro_cliente, name="registro")
 ]
